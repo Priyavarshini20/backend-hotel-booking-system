0f3d8e9ab1d7809559886a9c84931e1a61ab5115
A.P.Priyavarshini <priyavarshini383@gmail.com>
2024-12-05 13:21:54 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,8 +22,8 @@ const app = express();
 const connect = async () => {
 
     try {
-        // await mongoose.connect(process.env.MONGO);
-        await mongoose.connect("process.env.MONGO/booking");
+         await mongoose.connect(process.env.MONGO);
+        //await mongoose.connect("process.env.MONGO");
     } catch (error) {
         throw error;
     }
